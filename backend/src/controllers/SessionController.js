a784739a3d2aff2815bcb8c069311087a8c3847c
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-07-26 16:42:49 -0300

    implemented simplified login

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,14 +1,14 @@
 const User = require ('../models/User');
 
 module.exports = {
-    async index(req, res){
+    async store(req, res){
         const{email} = req.body;
         const{password} = req.body;
         let user = await User.findOne({email});
         let pass = await User.findOne({password});
 
         if (!user || !pass){
-            return res.json('User and/or password incorrect');
+            
         }
         return res.json(user, pass);
     }
