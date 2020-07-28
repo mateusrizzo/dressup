3ed412b317c587f8df58ae3de331482de99d32e9
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-07-28 12:27:00 -0300

    Dashboard tested and working

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -2,7 +2,7 @@ const Item = require('../models/Item');
 
 module.exports = {
     async show(req, res){
-        const user_id = req.headers;
+        const {user_id} = req.headers;
 
         const items = await Item.find({user: user_id});
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,12 +3,13 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const ItemController = require('./controllers/ItemController');
+const DashboardController = require('./controllers/DashboardController');
 
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 routes.post('/session', SessionController.store);
 routes.post('/item', upload.single('thumbnail'), ItemController.store);
-
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
