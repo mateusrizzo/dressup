d0e9cc7848b369adc1d4aba1666a66a6c29ac213
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-07-28 12:20:47 -0300

    Dashboard Controller created

---
+++ backend/src/controllers/DashboardController.js
@@ -0,0 +1,11 @@
+const Item = require('../models/Item');
+
+module.exports = {
+    async show(req, res){
+        const user_id = req.headers;
+
+        const items = await Item.find({user: user_id});
+
+        return res.json(items)
+    }
+}
