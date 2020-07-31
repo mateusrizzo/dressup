7d8af5d23546f071d777209fda1aeb4f810da78a
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-07-31 16:38:57 -0300

    Changed admin route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,8 +8,8 @@ const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
-routes.post('/session', AdminController.store);
-routes.get('/session', AdminController.show)
+routes.post('/admin', AdminController.store);
+routes.get('/admin', AdminController.show)
 routes.post('/item', upload.single('thumbnail'), ItemController.store);
 routes.get('/dashboard', DashboardController.show);
 
